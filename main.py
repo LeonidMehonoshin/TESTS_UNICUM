c3096123f2c8b1148c6d90f676492021f0e59546
LeonidMehonoshin <mehonoshin.leonid1@yandex.ru>
2025-11-08 19:06:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import time, random
+import time, random, re
+from datetime import datetime
 
 def choice_pizza(number_error):
-    total = None
     size = 1
+
+    pizza_types = [
+        'Неаполитанская',
+        'Кальцоне',
+        'Римская',
+        'В нарезке',
+        'Сицилийская',
+        'Кастомная'
+
+    ]
 
     print(
         "\n############\n"
@@ -19,7 +29,7 @@ def choice_pizza(number_error):
         '\n#########################\n'
         '#       ПИЦЦА>> ____    #\n'
         '#           O  |    |   #\n'
-        '#          ||\\ |.   |    #\n'
+        '#          ||\\ |.   |   #\n'
         '#          ,,  |____|   #\n'
         '#########################\n'
     )
@@ -35,10 +45,36 @@ def choice_pizza(number_error):
         '#########################\n'
         '\nПицца на ваш выбор:\n'
     )
+    day = datetime.now().day
+    month = datetime.now().month
+    year = datetime.now().year
+
+    phone_num = email = ''
+    email_code = str(random.randint(0, 10))
+    phone_num_code = str(random.randint(0, 10))
+    user_code = -1
+    while (
+        not re.search(r'\D+.\D+', email)
+        and not re.search(r'\D+.\d+', email)
+        and not re.search(r'\d+.\D+', email)
+        and not re.search(r'\D+.\D', email)
+        and not re.search(r'\D+.\d', email)
+        and not re.search(r'\d+.\D', email)
+    ): email = input('Email: ')
+    while user_code != email_code: user_code = input('Введите код, отправленный на почту (от 0 до 10): ')
+    while (
+        not re.search(r'\D\d+\D\d\d\d\D\d\d\d\D\d\d\D\d\d', phone_num)
+        and not re.search(r'\d+\D\d\d\d\D\d\d\d\D\d\d\D\d\d', phone_num)
+        and not re.search(r'\d+\d\d\d\d\d\d\d\d\d\d', phone_num)
+        and not re.search(r'\D\d\D\d\d\d\D\d\d\d\D\d\d\D\d\d', phone_num)
+        and not re.search(r'\d\D\d\d\d\D\d\d\d\D\d\d\D\d\d', phone_num)
+        and not re.search(r'\d\d\d\d\d\d\d\d\d\d\d', phone_num)
+    ): phone_num = input('Номер телефона: ')
+    while user_code != phone_num_code: user_code = input('Введите код, отправленный на номер (от 0 до 10): ')
 
     years = number_error({
-        'text': 'Сколько вам лет(3-2000)\n: ',
-        'min': 3, 'max': 200
+        'text': 'Год рождения\n: ',
+        'min': year - 2000, 'max': year - 3
     })
 
     if years >= 18:
@@ -51,76 +87,81 @@ def choice_pizza(number_error):
         pizza_type = number_error({
             'text':(
                 '\nНазвание и цена:\n'
-                '1) Неаполитанская(1$)\n'
-                '2) Кальцоне(2$)\n'
-                '3) Римская(3$)\n'
-                '4) В нарезке(4$)\n'
-                '5) Сицилийская(5$)\n'
-                '6) Кастомная!(999$)\n: '
+                '0) Неаполитанская(1$)\n'
+                '1) Кальцоне(2$)\n'
+                '2) Римская(3$)\n'
+                '3) В нарезке(4$)\n'
+                '4) Сицилийская(5$)\n'
+                '5) Кастомная!(999$)\n: '
             ),
-            'min': 1, 'max': 6
+            'min': 0, 'max': 5
         })
 
     elif size == 2:
         pizza_type = number_error({
             'text':(
                 '\nНазвание и цена:\n'
-                '1) Неаполитанская(2$)\n'
-                '2) Кальцоне(4$)\n'
-                '3) Римская(6$)\n'
-                '4) В нарезке(8$)\n'
-                '5) Сицилийская(10$)\n'
-                '6) Кастомная!(1998$)\n: '
+                '0) Неаполитанская(2$)\n'
+                '1) Кальцоне(4$)\n'
+                '2) Римская(6$)\n'
+                '3) В нарезке(8$)\n'
+                '4) Сицилийская(10$)\n'
+                '5) Кастомная!(1998$)\n: '
             ),
-            'min': 1, 'max': 6
+            'min': 0, 'max': 5
         })
 
-    if pizza_type == 6:
+    if pizza_type == 5:
         meat = number_error({
             'text': '\n\nСколько добавить мяса?(в попугаях 1-10)\n: ',
             'min': 1, 'max': 10
         })
         green = number_error({
-            'text': '\nСколько добавить зелени?(в попугаях 1-10)\n: ',
+            'text': '\nСколько добавить зелени?(в травах 1-10)\n: ',
             'min': 1, 'max': 10
         })
         ketchnnaise = number_error({
             'text': '\nСколько добавить ketchnnaise?(в Ʞетчунезахъ 1-10)\n: ',
             'min': 1, 'max': 10
         })
+        cheese = number_error({
+            'text': '\nСколько добавить сыра?(в крысах 1-10)\n: ',
+            'min': 1, 'max': 10
+        })
+        mushroom = number_error({
+            'text': '\nСколько добавить грибочков?(в мухоморах 1-10)\n: ',
+            'min': 1, 'max': 10
+        })
 
-    if size == 2:
-        if pizza_type == 1: pizza_type, total = 'Неаполитанская', '2$'
-        elif pizza_type == 2: pizza_type, total = 'Кальцоне', '4$'
-        elif pizza_type == 3: pizza_type, total = 'Римская', '6$'
-        elif pizza_type == 4: pizza_type, total = 'В нарезке', '8$'
-        elif pizza_type == 5: pizza_type, total = 'Сицилийская', '10$'
-    else:
-        if pizza_type == 1: pizza_type, total = 'Неаполитанская', '1$'
-        elif pizza_type == 2: pizza_type, total = 'Кальцоне', '2$'
-        elif pizza_type == 3: pizza_type, total = 'Римская', '3$'
-        elif pizza_type == 4: pizza_type, total = 'В нарезке', '4$'
-        elif pizza_type == 5: pizza_type, total = 'Сицилийская', '5$'
+    total_types = [1, 2, 3, 4, 5, 999]
+    if size == 2: total_types = [2, 4, 6, 8, 10, 1998]
+    print(pizza_type)
+    total = total_types[pizza_type]
+    pizza_type_text = pizza_types[pizza_type]
 
-    print(f'\n.........Кассовый чек №{random.randint(0,2000)}........')
+    print(f'            {day}.{month}.{year}'
+          f'\n.........Кассовый чек №{random.randint(0,2000)}........')
 
-    if pizza_type == 6:
-        if size == 1: total = '999$'
-        else: total = '1998$'
-
+    if pizza_type == 5:
         print(
             f'Название: Кастомная:\n'
-            f'  мясо: {meat}, зелень: {green}\n'
-            f'  кетчунез: {ketchnnaise}'
+            f'  мясо: {meat}\n'
+            f'  зелень: {green}\n'
+            f'  кетчунез: {ketchnnaise}\n'
+            f'  сыра: {cheese}\n'
+            f'  грибочков: {mushroom}'
         )
-    else: print(f'Название: {pizza_type}')
-    if years >= 18: print(f'Размер пиццы: {size}')
+    else: print(f'Название: {pizza_type_text}')
+    if years <= year - 18: print(f'Размер пиццы: {size}')
+    print(f'Год рождения: {year}\n')
+    print(f'Email: {email}')
+    print(f'Номер телефона: {phone_num}')
 
     print(
-        f'ИТОГО: {total}\n'
-        '\n||||| |||||||||| |||||\n\n'
+        f'ИТОГ: {total}$\n'
+        '|| ||| ||||| ||||| ||| ||\n'
         f'........СПАСИБО ЗА ПОКУПКУ!.......\n'
-        '  сеть пиццерий "Грустная пицца"\n'
+        '  сеть пиццерий "Грустная пицца"'
     )
 
 @choice_pizza
@@ -128,10 +169,7 @@ def number_error(options):
     while True:
         try:
             num = int(input(options['text']))
-            if num <= options['max'] and num >= options['min']:
-                break
-            else:
-                print('\nПроверьте условие ввода числа!\n')
-        except ValueError:
-            print('\nНеверный формат данных\n')
+            if num <= options['max'] and num >= options['min']: break
+            else: print('\nПроверьте условие ввода числа!\n')
+        except ValueError: print('\nНеверный формат данных\n')
     return num
