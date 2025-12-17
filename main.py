bad93fa93860226a3656cc61608e431f76474de7
LeonidMehonoshin <mehonoshin.leonid1@yandex.ru>
2025-12-18 01:55:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,107 +1,61 @@
-import random, time
-def main():
-    def test0():
-        print('test0')
+import time
 
-        n = int(input('Число n: '))
-        k = int(input('Число k: '))
+def towers_printer(towers):
+    for i in range(len(towers)):
+        print(f'{i+1} - ', end='')
+        for disk in towers[i]:
+            print(colors[disk-1], end=' ')
+        print()
+    print()
 
-        tic = time.perf_counter()
+def checker_true(towers, true_tower):
+    return len(towers[true_tower]) == 5 and towers[true_tower] == [1, 2, 3, 4, 5]
 
-        a = [random.randint(0, 20000) for i in range(n)]
-        a.sort()
-        resault = 0 if a[0] >= k else n+1
-        print(a)
+def checker_move(a, b):
+    if len(a) == 0: return False
+    if len(b) == 0: return True
+    return a[0] < b[0]
 
-        for i in range(n-1):
-            if a[i-1] <= k < a[i]: resault = i; break
-            elif a[i] == k: resault = i-1; break
+def prediction(towers):
+    predictions = []
+    for a in range(3):
+        for b in range(3):
+            if a != b and checker_move(towers[a], towers[b]):
+                new = [tower.copy() for tower in towers]
+                disk = new[a].pop(0)
+                new[b].insert(0, disk)
+                predictions.append(new)
+    return predictions
 
-        print(resault)
+colors = [
+    "\033[33m1\033[0m", "\033[31m2\033[0m", "\033[32m3\033[0m",
+    "\033[36m4\033[0m", "\033[35m5\033[0m"
+]
 
-        toc = time.perf_counter()
-        print(f'Вычисление заняло: {toc - tic:0.4f} секунд\n')
+while True:
+    towers = []
+    print('\nВводите любые числа (от 1 до 5) через пробел(пример: 1 2 3 4 5) [верх|низ]\n')
+    try:
+        towers.append([int(disk) for disk in input(f'Диски на башне 1 - ').split()])
+        towers.append([int(disk) for disk in input(f'Диски на башне 2 - ').split()])
+        towers.append([int(disk) for disk in input(f'Диски на башне 3 - ').split()])
+        true_tower = int(input("На какую башню собрать? (1 / 2 / 3): ")) - 1
+        if (sum(map(sum, towers)) == 15 and
+            len(towers[0]) + len(towers[1]) + len(towers[2]) == 5
+            and true_tower in (0, 1, 2)): break
+    except ValueError: pass
 
-    def test1():
-        print('test1')
+inbox = [(towers, [])]
+checked = set([str(towers)])
 
-        n = int(input('Число n: '))
-        k = int(input('Число k: '))
+while inbox:
+    current_towers, result = inbox.pop(0)
+    if checker_true(current_towers, true_tower):
+        break
+    for next_towers in prediction(current_towers):
+        if str(next_towers) not in checked:
+            checked.add(str(next_towers))
+            inbox.append((next_towers, result + [next_towers]))
 
-        tic = time.perf_counter()
-
-        b = [0, n-1]
-
-        a = [random.randint(0, 20000) for i in range(n)]
-        a.sort()
-        resault = 0 if a[0] >= k else n+1
-        print(a)
-
-        while b[0] <= b[1]:
-            center = (b[0] + b[1]) // 2
-            if center < n-1 and a[center-1] <= k < a[center]:
-                resault = center; break
-            elif a[center] < k: b[0] = center + 1
-            else: b[1] = center - 1
-
-        print(resault)
-
-        toc = time.perf_counter()
-        print(f'Вычисление заняло: {toc - tic:0.4f} секунд\n')
-
-    def test2():
-        print('test2')
-
-        n = int(input('Число n: '))
-        k = int(input('Число k: '))
-
-        tic = time.perf_counter()
-
-        a = [random.randint(0, 20000) for i in range(n)]
-        a.sort()
-        resault = 0 if a[0] >= k else n+1
-        print(a)
-
-        def rec(a, k, n, i=0):
-            if i >= n-1 or len(a) <= 2: return 0
-            elif a[i-1] <= k < a[i]: return i
-            elif a[i] == k: return i
-            return rec(a, k, n, i+1)
-
-        try:
-            resault = rec(a, k, n)
-            print(resault)
-
-        except RecursionError: print('Превышен лимит рекурсии')
-
-        toc = time.perf_counter()
-        print(f'Вычисление заняло: {toc - tic:0.4f} секунд\n')
-
-    def super_hyper_tester3000():
-        print('super_hyper_tester3000');n=int(input('Число n: '));k=int(input('Число k: '));tic=time.perf_counter();a=[]
-        for i in range(n):a.append(random.randint(0,20000))
-        for i in range(n):
-          for j in range(0,n-i-1):
-            if a[j]>a[j+1]:bugcup=a[j];a[j]=a[j+1];a[j+1]=bugcup
-        if a[0]>=k:resault=len(a)+1
-        else:resault=0
-        print(a);ababababa=-1
-        for i in range(len(a)):
-          if a[i]==k:ababababa=i;print(ababababa);break;
-        if ababababa!=-1:pass
-        else:
-          a.append(k);n = n+1
-          for i in range(n):
-            for j in range(0,n-i-1):
-              if a[j]>a[j+1]:bugcup=a[j];a[j]=a[j+1];a[j+1]=bugcup
-        while True:
-          c=random.randint(0,n-1);
-          if a[c]==k:print(c);break
-        toc=time.perf_counter()
-        print('Вычисление заняло:'+' '+str(round(toc-tic,4))+' '+'секунд')
-
-    test0()
-    test1()
-    test2()
-    super_hyper_tester3000()
-main()
+print(); towers_printer(towers)
+for step in result: time.sleep(1); towers_printer(step)
